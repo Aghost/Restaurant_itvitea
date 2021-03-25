cb0906852b07f9f6a54605d41de5c6d27ee85346
noname <no@email.com>
2021-03-25 15:25:24 +0100

    init commit

---
+++ Restaurant/Models/Order.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Restaurant.Models
+{
+    public class Consumeable
+    {
+        public int id { get; set; }
+        public bool isReady { get; set; }
+    }
+
+    public class Order
+    {
+        public int id { get; set; }
+        public int tableId { get; set; }
+        public List<Consumeable> foodIds { get; set; }  // tussentabel
+        public List<Consumeable> drinkIds { get; set; } // tussentabel
+        public int totalPrice { get; set; }
+        public bool completed { get; set; }
+    }
+}
